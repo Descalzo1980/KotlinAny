b754cead8388bc6ff99f10fc63475737db82b431
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-20 22:04:03 +0300

    Initial commit

---
+++ Arrays/Arrays.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val numbers = arrayOf("one", "two", "three", "four")
+//    for (item in numbers) {
+//        repeat(2){
+//            println(numbers.size)
+//        }
+//
+//    }
+    println("First element : ${numbers[8]}")
+}
