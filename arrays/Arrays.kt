dfbcb868aefc090a5c41fbf43f2fe702cedcfe90
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-20 23:00:53 +0300

    add

--- arrays/Arrays.kt
+++ arrays/Arrays.kt
@@ -1,22 +1,31 @@
 package arrays
 
-import kotlin.reflect.typeOf
-
 fun main() {
 
-    val names = arrayOf("one", "two", "three", "four")
+  val bigNumb = arrayOf(1,2,3,4,5,6,7)
+    println(findMax(bigNumb))
+    println(findMin(bigNumb))
 
-    val numbers = arrayOf(1,3,4,5,5,"String","Alex","Boo",'a')
-    names[0] = "five"
-/*    println("First element : ${names[0]}")
-    println("First element : ${names.size-1}")*/
 
-    for (i in numbers){
-        if (i is Int){
-            println("Это числа $i")
-        }else if (i is String){
-            println("Это строки $i")
+}
+fun findMax(numbers: Array<Int>) : Int{
+    var max = numbers[0]
+
+    for (number in numbers){
+        if (number > max){
+            max = number
         }
-
     }
+    return max
+}
+
+fun findMin(numbers: Array<Int>) : Int{
+    var min = numbers[0]
+
+    for (number in numbers){
+        if (number < min){
+            min = number
+        }
+    }
+    return min
 }
