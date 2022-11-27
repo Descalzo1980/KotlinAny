160404c4ecf1da2bdb3315149f4d5817cdab4276
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-27 21:24:10 +0300

    getter and setter

--- OOP/CodeWar.kt
+++ OOP/CodeWar.kt
@@ -2,13 +2,7 @@ package OOP
 
 fun removeChar(str: String): String = str.substring(1,str.lastIndex)
 fun main() {
-
+    val result = removeChar("Hello").apply {
+        println(message = this)
+    }
 }
-
-//fun removeChar(str: String): String {
-//    var result = str
-//    var drop = result.first()
-//    var drop2 = result.last()
-//    var myStr : String = removeChar(str.get(0),result.length-1)
-//    return str
-//}
--- OOP/Main.kt
+++ OOP/Main.kt
@@ -4,37 +4,41 @@ fun main() {
 
 
     val user = User(firstName = "Alex")
-    val friend = User("john","Smith")
-    val worker = User("Boo","Li",99)
-    val user2 = User(age = 19, firstName = "Iona", lastName = "Debbi")
+//    val friend = User("john","Smith")
+//    val worker = User("Boo","Li",99)
+//    val user2 = User(age = 19, firstName = "Iona", lastName = "Debbi")
 
-    println("Name =  ${user.firstName}")
-    println("LastName =  ${user.lastName}")
-    println("Age =  ${user.age}")
-
-    println("\n")
-
-    println("Name =  ${friend.firstName}")
-    println("LastName =  ${friend.lastName}")
-    println("Age =  ${friend.age}")
-
-    println("\n")
-
-    println("Name =  ${worker.firstName}")
-    println("LastName =  ${worker.lastName}")
-    println("Age =  ${worker.age}")
-
-
-
-
+    println(user.firstName)
+    println(user.lastName)
+    println(user.age)
+//
+//    println("\n")
+//
+//    println("Name =  ${friend.firstName}")
+//    println("LastName =  ${friend.lastName}")
+//    println("Age =  ${friend.age}")
+//
+//    println("\n")
+//
+//    println("Name =  ${worker.firstName}")
+//    println("LastName =  ${worker.lastName}")
+//    println("Age =  ${worker.age}")
 }
 
-class User(firstName: String, var lastName:String = "LastName", var age:Int = 0){
+class User(firstName: String,lastName:String = "LastName", var age:Int = 0){
     var firstName = firstName
         get() {
-            return field
+            return "FirstName: $field"
         }
         set(value) {
+            println("$value was assigned to firstName property")
             field = value
+        }
+    var lastName = lastName
+        get() {
+            return "Last name : $field"
+        }
+        set(value) {
+            println("$value was assigned to lastName property")
         }
 }
