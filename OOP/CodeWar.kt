15d64457742effcb05aebf9656f8824430b67cf4
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-27 21:16:29 +0300

    getter and setter

---
+++ OOP/CodeWar.kt
@@ -0,0 +1,14 @@
+package OOP
+
+fun removeChar(str: String): String = str.substring(1,str.lastIndex)
+fun main() {
+
+}
+
+//fun removeChar(str: String): String {
+//    var result = str
+//    var drop = result.first()
+//    var drop2 = result.last()
+//    var myStr : String = removeChar(str.get(0),result.length-1)
+//    return str
+//}
--- OOP/Main.kt
+++ OOP/Main.kt
@@ -1,12 +1,40 @@
 package OOP
 
 fun main() {
-    val user = User("Alex")
-    val friend = User("john","Smith",40)
+
+
+    val user = User(firstName = "Alex")
+    val friend = User("john","Smith")
+    val worker = User("Boo","Li",99)
+    val user2 = User(age = 19, firstName = "Iona", lastName = "Debbi")
+
+    println("Name =  ${user.firstName}")
+    println("LastName =  ${user.lastName}")
+    println("Age =  ${user.age}")
+
+    println("\n")
+
+    println("Name =  ${friend.firstName}")
+    println("LastName =  ${friend.lastName}")
+    println("Age =  ${friend.age}")
+
+    println("\n")
+
+    println("Name =  ${worker.firstName}")
+    println("LastName =  ${worker.lastName}")
+    println("Age =  ${worker.age}")
+
+
+
 
 }
 
-class User(var name: String,var lastName:String,var age:Int){
-
-    constructor(name : String) : this(name ,"Last Name",0)
+class User(firstName: String, var lastName:String = "LastName", var age:Int = 0){
+    var firstName = firstName
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
 }
