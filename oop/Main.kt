bcbe8ef29ee8c5002246db7d26bd981721c58ff1
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-28 12:46:05 +0300

    oop.Database@135fbaa4 oop.Database@135fbaa4 oop.Database@135fbaa4 oop.Database@135fbaa4 oop.Database@135fbaa4 oop.Database@135fbaa4

--- oop/Main.kt
+++ oop/Main.kt
@@ -1,20 +1,18 @@
 package oop
 
 fun main() {
-    val instance = Database.getInstance()
-    val instance2 = Database.getInstance()
-    println(instance)
-    println(instance2)
+    println(Database)
+    println(Database)
+    println(Database)
+    println(Database)
+    println(Database)
+    println(Database)
+    println(Database)
+    println(Database)
 }
 
-class Database private constructor(){
-    companion object{
-        private var instance: Database? =null
-        fun getInstance(): Database? {
-            if(instance == null){
-                instance = Database()
-            }
-            return instance
-        }
+object Database{
+    init {
+        println("Database create")
     }
 }
