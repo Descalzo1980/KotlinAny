db444ebe000f6a22d53759ef8980d5063e1cd118
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-04 00:23:48 +0300

    data class,hash,equals. all generate auto

--- data/User.kt
+++ data/User.kt
@@ -1,27 +1,5 @@
 package data
 
-data class User(val firstName : String,val lastName : String,val age : Int ){
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other){
-            return true
-        }
-        if (other is User){
-            return this.firstName == other.firstName
-                    && this.lastName == other.lastName
-                    && this.age == other.age
-        }
-        return false
-    }
-
-    override fun hashCode(): Int {
-        var result = firstName.hashCode()
-        result = 31 * result + lastName.hashCode()
-        result = 31 * result + age
-        return result
-    }
-
-    override fun toString(): String {
-        return "User(firstName = $firstName,lastName = $lastName,age = $age)"
-    }
+data class User(val firstName : String,val lastName : String){
+    var age = 0
 }
--- data/main.kt
+++ data/main.kt
@@ -2,13 +2,12 @@ package data
 
 fun main() {
 
-    val userFirst = User("Alex","Boo",23)
-    val userSecond = User("John","Boo",23)
-
-    println(userFirst == userSecond)
+    val userFirst = User("Alex","Boo")
+    val userSecond = User("Alex","Boo")
 
     println(userFirst.hashCode())
     println(userSecond.hashCode())
+
 
     println(userFirst)
 }
