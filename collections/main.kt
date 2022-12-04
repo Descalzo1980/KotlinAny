37c4e52fb21039a6a5336a36d11b70417c0c2f73
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-04 19:56:45 +0300

    collections

--- collections/main.kt
+++ collections/main.kt
@@ -3,22 +3,31 @@ package collections
 import java.util.function.Predicate
 
 fun main() {
+    val user = User("Name 1")
+    val user1 = User("Name 2")
+    val user2 = User("Name 3")
+    val user3 = User("Name 4")
+    val user4 = User("Name 5")
+    val user5 = User("Name 6")
+    val user6 = User("Alex")
+    val user7 = User("Alex")
 
-    val names = mutableListOf("Name 1","Name 2","Name 3")
-    names.add(1,"Name 5")
-    val newName = Predicate { name:String -> name == "Name 5" || name == "Name 1"}
-//    remove(names,newName)
-//    println(names)
+    val names = mutableListOf("Name 1","Name 2","Name 3","Name 3")
+    val names1 = mutableSetOf<String>("Name 1","Name 2","Name 3","Name 3")
+    val userNames = mutableSetOf<User>(user,user1,user2,user3,user4,user5,user6,user7)
+//    names.add(1,"Name 5")
+//    names.removeAt(3)
+//    names.forEach { name -> println(name) }
+//    println("*********************")
+//    names.forEach { println(it) }
+//    println("*********************")
+//    names1.forEach { println(it) }
+    println("*********************")
+    userNames.forEach { println(it.name) }
 
-    removeAll(names,newName)
-    println(names)
-
-//    for (name in names){
-//        println(name)
-//    }
-//
-//    println(names[1])
 }
+data class User(val name: String) // если не data class то дает положить в set одинаковые значения, equals не переписан
+
 fun <T> remove(list: MutableList<T>,predicate: Predicate<T>){
     list.removeIf{x:T->predicate.test(x)}
 }
