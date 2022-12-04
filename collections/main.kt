85e984a1c25cce989c5a76a684f8bc20af1ea516
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-04 19:42:25 +0300

    collections

--- collections/main.kt
+++ collections/main.kt
@@ -6,16 +6,25 @@ fun main() {
 
     val names = mutableListOf("Name 1","Name 2","Name 3")
     names.add(1,"Name 5")
-    var newName = Predicate { name:String -> name == "Name 5" }
-    remove(names,newName)
+    val newName = Predicate { name:String -> name == "Name 5" || name == "Name 1"}
+//    remove(names,newName)
+//    println(names)
+
+    removeAll(names,newName)
     println(names)
 
-    for (name in names){
-        println(name)
-    }
-
-    println(names[1])
+//    for (name in names){
+//        println(name)
+//    }
+//
+//    println(names[1])
 }
 fun <T> remove(list: MutableList<T>,predicate: Predicate<T>){
     list.removeIf{x:T->predicate.test(x)}
+}
+
+fun<T> removeAll(list: MutableList<T>,predicate: Predicate<T>){
+    val newList: MutableList<T> = ArrayList()
+    list.filter { predicate.test(it) }.forEach { newList.add(it) }
+    list.removeAll(newList)
 }
