62567324371327ce623ab1bbeae3335b1869cb8e
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-05 18:18:58 +0300

    unzip

--- collection_operation/main.kt
+++ collection_operation/main.kt
@@ -1,5 +1,6 @@
 package collection_operation
 
+import java.beans.Expression
 import java.util.*
 
 
@@ -26,4 +27,15 @@ fun main() {
     val res1 = (colors.zip(animals){color,animal-> "The ${animal.replaceFirstChar { it.uppercase() }} is $color"})
     println(res1)
 
+    val str1 = "ABCCCVVBBGG"
+    val str2 = "VBB".toSet()
+    fun filterString(expression: String) = expression.filter { it in str2}
+    val filtered = filterString(str1)
+    println(filtered)
+    val res2 = str1.filter { it -> it.isDigit() }
+    println("Filtered String : $res2")
+
+    val numbersPairs = listOf("one" to 1,"two" to 2,"three" to 3)
+    println(numbersPairs)
+    println(numbersPairs.unzip())
 }
