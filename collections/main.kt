6fe8bf69f6c5e42a9b085af514d455c6686fe728
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-05 17:05:48 +0300

    map

--- collections/main.kt
+++ collections/main.kt
@@ -3,7 +3,12 @@ package collections
 
 fun main() {
     
-    val users = mapOf<Int,String>(1 to "Alex",2 to "Boo",3 to "Oui")
+    val users = mutableMapOf<Int,String>(1 to "Alex",2 to "Boo",3 to "Oui")
     
-    users.forEach { (t, u) ->  println("$t : $u") }
+
+    users[4] = "John"
+    users.forEach { (k, v) ->  println("$k and $v") }
+    users.remove(1)
+    println("************************************")
+    users.forEach { (k, v) ->  println("$k and $v") }
 }
