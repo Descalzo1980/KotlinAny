8a19737e6dce5fc942b3977c430c3af8d7b2b00e
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-05 19:41:51 +0300

    multiple arrays

--- collection_operation/main.kt
+++ collection_operation/main.kt
@@ -6,5 +6,11 @@ fun main() {
     val numbers = listOf("one","two","three","four")
     println(numbers.associateWith { it.length })
     println(numbers.associateBy { it.first().uppercase() })
-    println(numbers.associateBy(keySelector = {it.first().uppercase()}))
+    println(numbers.associateBy(keySelector = {it.first().uppercase()}, valueTransform = {it.length}))
+
+    /*
+    * flatten*/
+
+    val numberSets = arrayOf(arrayOf(1,2,3), arrayOf(4,5,6), arrayOf(7,8,9))
+    println(numberSets[2][2])
 }
