82dd58ef4950f842c9ad4cbaf523b8a6f9e211ba
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-05 19:49:46 +0300

    flatten

--- collection_operation/main.kt
+++ collection_operation/main.kt
@@ -3,14 +3,17 @@ package collection_operation
 
 fun main() {
 
-    val numbers = listOf("one","two","three","four")
-    println(numbers.associateWith { it.length })
-    println(numbers.associateBy { it.first().uppercase() })
-    println(numbers.associateBy(keySelector = {it.first().uppercase()}, valueTransform = {it.length}))
-
     /*
-    * flatten*/
+     * flatten*/
 
-    val numberSets = arrayOf(arrayOf(1,2,3), arrayOf(4,5,6), arrayOf(7,8,9))
-    println(numberSets[2][2])
+    val numberSets = listOf(setOf(1, 2, 3), setOf(4, 5, 6), setOf(7, 8, 9))
+    for (numbers in numberSets) {
+        for (number in numbers) {
+            println(number)
+        }
+        println("\n")
+    }
+    val numbersFlatten = numberSets.flatten()
+    println(numbersFlatten)
 }
+
