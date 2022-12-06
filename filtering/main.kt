a9de816ce21d0a30978f4ebb8fd58cfe43283b44
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-06 10:11:29 +0300

    filterIsInstance

--- filtering/main.kt
+++ filtering/main.kt
@@ -1,24 +1,23 @@
 package filtering
 
 fun main() {
-    val numbers = listOf("one","two","three","four")
-//    val longerThanThree = numbers.filter { it.length > 3 }
-//    println(longerThanThree)
-//
-//    val numbersMap = mutableMapOf("key 1 " to 1,"key 2 " to 2, "key 3" to 3,"key 101" to 101)
-//    val filterMap = (numbersMap.filter { it.key.endsWith("1")}).toMutableMap()
-//    filterMap.remove("key 101")
-//    println(filterMap)
+    val numbers = listOf("one", "two", "three", "four")
+    val longerThanThree = numbers.filter { it.length > 3 }
+    println(longerThanThree)
 
-//    val filterIndex = numbers.filterIndexed { index, value ->  (index !=0 && (value.length < 5))}
-//    val filterNot = numbers.filterNot { it.length <=3 }
-//    println(filterIndex)
-//    println(filterNot)
+    val numbersMap = mutableMapOf("key 1 " to 1,"key 2 " to 2, "key 3" to 3,"key 101" to 101)
+    val filterMap = (numbersMap.filter { it.key.endsWith("1")}).toMutableMap()
+    filterMap.remove("key 101")
+    println(filterMap)
 
-    val res = loveFun(2,2)
-    println(res)
-}
+    val filterIndex = numbers.filterIndexed { index, value ->  (index !=0 && (value.length < 5))}
+    val filterNot = numbers.filterNot { it.length <=3 }
+    println(filterIndex)
+    println(filterNot)
 
-fun loveFun(flowerA: Int, flowerB: Int): Boolean {
-    return (flowerA + flowerB) % 2 == 1
+    val mixedList = listOf(1,2,3,4,5,'A',"A",'B',"B","Alex",true,false)
+    mixedList.filterIsInstance<Int>().forEach {
+        println(it)
+    }
+
 }
