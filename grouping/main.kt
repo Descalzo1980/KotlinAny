b3768a3cdc036075522dc4bd4eccef19cb7d5015
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-06 10:41:22 +0300

    groupBy

---
+++ grouping/main.kt
@@ -0,0 +1,7 @@
+package grouping
+
+fun main() {
+    val numbers = listOf("one","two","three","four")
+    println(numbers.groupBy { it.first().uppercase() })
+    println(numbers.groupBy(keySelector = {it.first()}, valueTransform = {it.uppercase()}))
+}
