43e4043f269908d622c2933e6ced781963dc2cb6
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-07 15:00:07 +0300

    comparator and comparable sortedWith!!!!

--- order/main.kt
+++ order/main.kt
@@ -25,6 +25,18 @@ fun main() {
     laptop.sorted().forEach { println(it) }
     println("\n")
     laptop.sortedWith(comparator = ComparatorRam()).forEach { println(it.ram) }
+    laptop.sortedWith(comparator = ComparatorYear()).forEach { _ -> }
+    /*
+    * or we can use lambda
+    * */
+    var sing = "*"
+
+    println(sing.repeat(80))
+    laptop.sortedWith(compareBy { it.price }).forEach { println(it) }
+    println(sing.repeat(80))
+    laptop.sortedWith(compareBy { it.year }).forEach { println(it) }
+    println(sing.repeat(80))
+    laptop.sortedWith(compareBy { it.brand }).forEach { println(it) }
 }
 
 data class Laptop(val brand: String,val year: Int,val ram: Int,val price: Int) : Comparable<Laptop>{
@@ -54,3 +66,19 @@ class ComparatorRam : Comparator<Laptop>{
     }
 
 }
+
+class ComparatorYear : Comparator<Laptop>{
+    override fun compare(laptop1: Laptop, laptop2: Laptop): Int {
+        return if (laptop1.year > laptop2.year){
+            println("У этого компа ${laptop1.brand} вот такой ${laptop1.year} год выпуска")
+            1
+        }else if(laptop1.year < laptop2.year){
+            println("У этого компа ${laptop1.brand} вот такой ${laptop1.year} год выпуска")
+            0
+        }else{
+            println("У этого компа ${laptop1.brand} вот такой ${laptop1.year} год выпуска")
+            0
+        }
+    }
+}
+
