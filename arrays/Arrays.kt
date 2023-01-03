d205427e3d15bf64a1dbeb1629028c9adcac68d7
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-03 10:37:45 +0300

    vars

--- arrays/Arrays.kt
+++ arrays/Arrays.kt
@@ -1,52 +1,6 @@
 package arrays
 
 fun main() {
-
-    val bigNumb = arrayOf(1, 2, 3, 4, 5, 6, 7)
-    println(findMinAndMax(bigNumb,searchMax = true))
-    println(findMinAndMax(bigNumb,searchMax = false))
-
-
+    var first: String
 }
 
-fun findMinAndMax(numbers: Array<Int>, searchMax: Boolean): Int {
-    var max = numbers[0]
-    var min = max
-    if (searchMax) {
-        for (number in numbers) {
-            if (number > max) {
-                max = number
-            }
-        }
-        return max
-    } else {
-        for (number in numbers) {
-            if (number < min) {
-                min = number
-            }
-        }
-    }
-    return min
-}
-
-/*fun findMax(numbers: Array<Int>) : Int{
-    var max = numbers[0]
-
-    for (number in numbers){
-        if (number > max){
-            max = number
-        }
-    }
-    return max
-}
-
-fun findMin(numbers: Array<Int>) : Int{
-    var min = numbers[0]
-
-    for (number in numbers){
-        if (number < min){
-            min = number
-        }
-    }
-    return min
-}*/
--- coroutines/coroutines.kt
+++ coroutines/coroutines.kt
@@ -1,42 +1,20 @@
 package coroutines
 
 import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
 
 
 fun main() {
-        runBlocking {
-            println("Weather forecast")
-            try {
-                println(getWeatherReport())
-            }catch (e: AssertionError){
-                println("Caught exception in runBlocking(): $e")
-                println("Report unavailable at this time")
+    runBlocking {
+        println("${Thread.currentThread().name} - runBlocking function")
+        launch {
+            println("${Thread.currentThread().name} - launch function")
+            withContext(Dispatchers.Default) {
+                println("${Thread.currentThread().name} - withContext function")
+                delay(1000)
+                println("10 results found.")
             }
-            println("Have a good day!")
+            println("${Thread.currentThread().name} - end of launch function")
         }
+        println("Loading...")
     }
-
-suspend fun getForecast() : String{
-    delay(1000)
-    return ("Sunny")
-}
-
-suspend fun getWeatherReport() = coroutineScope {
-    val forecast = async { getForecast() }
-    val temperature = async {
-        try {
-            getTemperature()
-        }catch (e: Exception){
-            println("Caught exception $e")
-            "{ No temperature found }"
-        }
-    }
-    "${forecast.await()} ${temperature.await()}"
-}
-
-suspend fun getTemperature(): String {
-    delay(500)
-    throw AssertionError("Temperature is invalid")
-    return ("30\u00b0C")
 }
