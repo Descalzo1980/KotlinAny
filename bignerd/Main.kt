4fdcfc7db173985a033ea81257ffaa137242a088
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-23 13:21:36 +0300

    всего по немногу

--- bignerd/Main.kt
+++ bignerd/Main.kt
@@ -1,19 +1,40 @@
 package bignerd
 
+
 const val HERO_NAME = "Madrigal"
+var playerLevel: Int = 0
+
 fun main() {
-    println("The hero announces her presence to the world.")
-    println(HERO_NAME)
-    var playerLevel = (0..8).random()
+    println("$HERO_NAME announces her presence to the world.")
+    println("What level is $HERO_NAME?")
+    val playerLevelInput = readln()
+    playerLevel = if (playerLevelInput.matches("""\d+""".toRegex())){
+        playerLevelInput.toInt()
+    }else{
+        1
+    }
+
+    println("$HERO_NAME's level is $playerLevel" )
+    obtainQuest(playerLevel)
+    readBountyBoard()
+    obtainQuest(playerLevel)
+    println("Time passes...")
+    println("$HERO_NAME returns from her quest.")
+    playerLevel += 1
     println(playerLevel)
-    val hasBefriendedBarbarians = true
-    val hasAngeredBarbarians = false
-    val playerClass = "paladin"
-    val quest: String = when (playerLevel) {
+    readBountyBoard()
+}
+
+fun obtainQuest(
+    playerLevel: Int,
+    playerClass: String = "paladin",
+    hasBefriendedBarbarians: Boolean = true,
+    hasAngeredBarbarians: Boolean = false,
+): String? = when (playerLevel) {
         1 -> "Meet Mr. Bubbles in the land of soft things."
         in 2..5 -> {
-            val canTalkToBarbarians = !hasAngeredBarbarians &&
-                    (hasBefriendedBarbarians || playerClass == "barbarian")
+            val canTalkToBarbarians = !hasBefriendedBarbarians &&
+                    (hasAngeredBarbarians || playerClass == "barbarian")
             if (canTalkToBarbarians) {
                 "Convince the barbarians to call off their invasion."
             } else {
@@ -23,12 +44,16 @@ fun main() {
         6 -> "Locate the enchanted sword."
         7 -> "Recover the long-lost artifact of creation."
         8 -> "Defeat Nogartse, bringer of death and eater of worlds."
-        else -> "There are no quests right now."
+        else -> null
     }
-    println("The hero approaches the bounty board. It reads:")
-    println(quest)
-    println("Time passes...")
-    println("The hero returns from her quest.")
-    playerLevel += 1
-    println(playerLevel)
+private fun readBountyBoard() {
+    val quest: String? = obtainQuest(playerLevel)
+    val message: String = quest?.replace("Nogartse", "xxxxxxxx")
+        ?.let { censoredQuest ->
+            """
+    |"$HERO_NAME approaches the bounty board. It reads:
+    |"$censoredQuest"
+    """.trimMargin()
+        } ?: "$HERO_NAME approaches the bounty board, but it is blank."
+    println(message)
 }
